65a6840150b36575c03cd451c2edee550ef2453d
John Blalock <simpledimplejohn@gmail.com>
2024-01-04 07:15:04 -0800

    update

--- learning/data_structures.py
+++ learning/data_structures.py
@@ -1,5 +1,33 @@
-# List
+## List [like arrays]
 list = [1,2,3]
 
-print(list)
+# print(list)
+
+
+## sets {distinct}
+my_set = {1,3,9,5,1}
+# print(my_set)
+
+## Dictionaryies {like objects}
+my_dictionary = {
+    'name' : 'john',
+    'age' : 25,
+    'stance' : 'open',
+    'upsidedown' : False
+}
+
+# print(my_dictionary)
+
+# temp = my_dictionary.update('name','joe')
+
+my_dictionary['name'] = 'fred'
+temp = my_dictionary['name']
+
+print('itmes:')
+print(my_dictionary.items())
+print('keys')
+print(my_dictionary.keys())
+print('values')
+print(my_dictionary.values())
+
 
--- learning/loops.py
+++ learning/loops.py
@@ -1,10 +1,21 @@
-# for loop
+## for loop
 list = [1,2,5,8,2,6,4,0]
 movelist = []
 
 for item in list:
-    print(item + 1)
+    # print(item + 1)
     movelist.append(item)
 
-print(movelist)
+# print(movelist)
+
+## dictionary loops
+my_dict = {
+    'a' : 1,
+    'b' : 2,
+    'c' : 5,
+    'd' : 8
+}
+
+for key in my_dict:
+    print(key, my_dict[key])
 
